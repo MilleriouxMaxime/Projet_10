453bf3158b9c78185c092ba4c86633dc1b52efb4
MilleriouxMaxime <milleriouxmaxime@gmail.com>
2025-05-03 20:39:38 +0200

    add test curl commands

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.core.exceptions import ValidationError
 from datetime import date
@@ -9,9 +9,28 @@ def validate_age(birth_date):
     if age < 15:
         raise ValidationError('Users must be at least 15 years old to register (GDPR compliance).')
 
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, birth_date=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not birth_date:
+            raise ValueError('The Birth Date field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, birth_date=birth_date, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, birth_date=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, birth_date, **extra_fields)
+
 class User(AbstractUser):
     birth_date = models.DateField(validators=[validate_age])
     email = models.EmailField(unique=True)
+    
+    objects = UserManager()
     
     def __str__(self):
         return self.username
