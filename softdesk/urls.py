eb31a40d9b043e749f3dbb91dc126921278f82a6
MilleriouxMaxime <milleriouxmaxime@gmail.com>
2025-05-20 16:11:14 +0200

    change to jwt

--- softdesk/urls.py
+++ softdesk/urls.py
@@ -14,13 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api/', include('users.urls')),
-    path('api/token/', obtain_auth_token, name='api_token_auth'),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("api/", include("users.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
